5d029b803248c6a1e466ffc6ce114e9b71edee6b
Vcmp <125277899+thr3343@users.noreply.github.com>
2023-03-05 17:24:27 +0000

    Use faster Depth format

--- src/main/java/net/vulkanmod/vulkan/Vulkan.java
+++ src/main/java/net/vulkanmod/vulkan/Vulkan.java
@@ -758,7 +758,7 @@ public class Vulkan {
 
     private static int findDepthFormat() {
         return findSupportedFormat(
-                stackGet().ints(VK_FORMAT_D24_UNORM_S8_UINT),
+                stackGet().ints(VK_FORMAT_D16_UNORM),
                 VK_IMAGE_TILING_OPTIMAL,
                 VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT);
     }
