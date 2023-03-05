50f1b2c16b802319cc58b5d167cf98d496ac6a17
Vcmp <125277899+thr3343@users.noreply.github.com>
2023-03-05 17:57:15 +0000

    Revert "Use faster Depth format"
    (Clear is actually used by Mojang to set colours)
    This reverts commit 5d029b803248c6a1e466ffc6ce114e9b71edee6b.

--- src/main/java/net/vulkanmod/vulkan/Vulkan.java
+++ src/main/java/net/vulkanmod/vulkan/Vulkan.java
@@ -535,12 +535,7 @@ public class Vulkan {
             createInfo.imageArrayLayers(1);
             createInfo.imageUsage(VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
 
-            QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
-
-            if(!indices.graphicsFamily.equals(indices.presentFamily)) {
-                createInfo.imageSharingMode(VK_SHARING_MODE_CONCURRENT);
-                createInfo.pQueueFamilyIndices(stack.ints(indices.graphicsFamily, indices.presentFamily));
-            } else {
+            {
                 createInfo.imageSharingMode(VK_SHARING_MODE_EXCLUSIVE);
             }
 
@@ -713,10 +708,10 @@ public class Vulkan {
 
         throw new RuntimeException("Failed to find supported format");
     }
-
+    //Nvidia and AMD only support either VK_FORMAT_D24_UNORM_S8_UINT or VK_FORMAT_D16_UNORM_S8_UINT respectively, (Not Both)
     private static int findDepthFormat() {
         return findSupportedFormat(
-                stackGet().ints(VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT),
+                stackGet().ints(VK_FORMAT_D16_UNORM_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT),
                 VK_IMAGE_TILING_OPTIMAL,
                 VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT);
     }
