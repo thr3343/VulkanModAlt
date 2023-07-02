be17a9cb370ea16f511bc8385a512b52edb14506
Vcmp <125277899+thr3343@users.noreply.github.com>
2023-07-02 13:23:06 +0100

    Revert "Update Drawer.java"
    This reverts commit fa800715ba4f95cb0c69e8de5ee15823a44a7be3.

--- src/main/java/net/vulkanmod/vulkan/Drawer.java
+++ src/main/java/net/vulkanmod/vulkan/Drawer.java
@@ -373,7 +373,7 @@ public class Drawer {
 
 
 
-            int vkResult = vkAcquireNextImageKHR(device, Vulkan.getSwapChain().getId(), 100000,
+            int vkResult = vkAcquireNextImageKHR(device, Vulkan.getSwapChain().getId(), VUtil.UINT64_MAX,
                     imageAvailableSemaphores.get(currentFrame), VK_NULL_HANDLE, pImageIndex);
 
 
