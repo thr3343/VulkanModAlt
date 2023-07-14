9958ff0ab29cb546040443fb96b5cfc547595ad1
Vcmp <125277899+thr3343@users.noreply.github.com>
2023-07-14 12:20:07 +0100

    Skip frame if VK_NOT_READY is returned

--- src/main/java/net/vulkanmod/vulkan/Drawer.java
+++ src/main/java/net/vulkanmod/vulkan/Drawer.java
@@ -398,10 +398,13 @@ public class Drawer {
 
 
 
-            int vkResult = vkAcquireNextImageKHR(device, Vulkan.getSwapChain().getId(), VUtil.UINT64_MAX,
+            int vkResult = vkAcquireNextImageKHR(device, Vulkan.getSwapChain().getId(), -1,
                     imageAvailableSemaphores.get(currentFrame), VK_NULL_HANDLE, pImageIndex);
 
 
+            if(vkResult == VK_NOT_READY) {
+                skipRendering=true; return;
+            }
             if(vkResult == VK_ERROR_OUT_OF_DATE_KHR || vkResult == VK_SUBOPTIMAL_KHR || shouldRecreate) {
                 shouldRecreate = false;
                 recreateSwapChain();
@@ -444,6 +447,9 @@ public class Drawer {
 
             vkResult = callPPI(getPresentQueue(), presentInfo.address(), device.getCapabilities().vkQueuePresentKHR);
 
+            if(vkResult == VK_NOT_READY ) {
+                skipRendering=true; return;
+            }
             if(vkResult == VK_ERROR_OUT_OF_DATE_KHR || vkResult == VK_SUBOPTIMAL_KHR || shouldRecreate) {
                 shouldRecreate = false;
                 recreateSwapChain();
