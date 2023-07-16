c072cefb379eea0f19881dc3e8f555cafbb49fef
Vcmp <125277899+thr3343@users.noreply.github.com>
2023-07-16 21:17:02 +0100

    Update DeviceInfo.java

--- src/main/java/net/vulkanmod/vulkan/DeviceInfo.java
+++ src/main/java/net/vulkanmod/vulkan/DeviceInfo.java
@@ -83,7 +83,7 @@ public class DeviceInfo {
 
         vkGetPhysicalDeviceFeatures2(this.device, this.availableFeatures);
 
-        this.depthAttachmentOptimal= hasDepthOnly() ? VK12.VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL : VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_STENCIL_READ_ONLY_OPTIMAL
+        this.depthAttachmentOptimal= hasDepthOnly() ? VK12.VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL : VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_STENCIL_READ_ONLY_OPTIMAL;
 
         this.hasLoadStoreOpNone= device.getCapabilities().Vulkan13;
 
